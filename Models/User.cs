4e5bb4de182ecdb669a0f653928d122b4ad2d305
pedroforadori <foradorif@gmail.com>
2023-10-27 20:40:02 -0300

    style: alteracao de codigo

--- Models/User.cs
+++ Models/User.cs
@@ -10,9 +10,9 @@ public class User
     public Guid Id { get; set; }
     public string? Fullname { get; set; }
     public required string Email { get; set; }
-    public required string Whatsapp { get; set; }
+    public string? Whatsapp { get; set; }
     public required string Password { get; set; }
-    public required EType Type { get; set; }
+    public EType? Type { get; set; }
     public DateTime? CreatAt { get; set; } = DateTime.Now;
     public ICollection<Project>? Projects { get; } = new List<Project>();
 }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddCors();
+
 var app = builder.Build();
+app.UseCors(builder => builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+);
 
 app.MapGet("/", () => "API ON");
-
 app.MapUserEndpoint();
 app.MapAuthEndpoint();
 app.MapProjectEndpoint();
